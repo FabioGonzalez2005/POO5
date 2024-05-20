b6868ed942bdc91c0ba7d79b9399215f4e6e39e2
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-20 12:41:01 +0100

    Metemos if y else en la función

--- textual1/button.py
+++ textual1/button.py
@@ -2,7 +2,6 @@ from textual.app import App, ComposeResult
 from textual.containers import Horizontal, VerticalScroll
 from textual.widgets import Button, Static
 
-
 class ButtonsApp(App[str]):
     CSS_PATH = "button.tcss"
 
@@ -10,20 +9,20 @@ class ButtonsApp(App[str]):
         yield Horizontal(
             VerticalScroll(
                 Static("Saludar al mundo", classes="header"),
-                Button("Saludar"),
-                Button.error("Salir"),
+                Button("Saludar", id="saludar"),
+                Button.error("Salir", id="salir"),
             ),
         )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
-        ##self.exit(str(event.button))
-        self.notify(
-            "Desde el IES Haría"
-            "[b]El 1º Ciclo del Ciclo Superior[/b] saluda al mundo!",
-            title="Saludo",
-            severity="warning",
-        )
-
+        if event.button.id == "saludar":
+            self.notify(
+                "Desde el IES Haría\n[b]El 1º Ciclo del Ciclo Superior[/b] saluda al mundo!",
+                title="Saludo",
+                severity="warning",
+            )
+        elif event.button.id == "salir":
+            self.exit("Saliendo")
 
 if __name__ == "__main__":
     app = ButtonsApp()
