a232bdc621c741bb8b7f3712440c57c4d3066378
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-21 13:02:27 +0100

    Adaptamos los test de test_tarea para el delimitador

--- hgt/tests/test_tarea.py
+++ hgt/tests/test_tarea.py
@@ -1,23 +1,18 @@
 import pytest
-from tarea import Tarea
+from hgt.tarea import Tarea
 
 @pytest.fixture
 def tarea():
     return Tarea(1, 'Mates', False)
 
-def test_tarea(tarea):
-    assert tarea.read() == '1, Mates, False'
-
 def test_read(tarea):
-    assert tarea.read() == '1, Mates, False'
+    assert tarea.read() == "1|&&|Mates|&&|False"
 
-def test_update(idNueva, tareaNueva, estadoNuevo):
-    idNueva.update('2')
-    tareaNueva.update('Lengua')
-    estadoNuevo.update('True')
+def test_update(tarea):
+    tarea.update(2, 'Lengua', True)
 
-    assert tarea.read() == '2, Lengua, True'
+    assert tarea.read() == "2|&&|Lengua|&&|True"
 
 def test_delete(tarea):
     tarea.delete()
-    assert tarea.read() == None
+    assert tarea.read() == "None|&&|None|&&|None"
