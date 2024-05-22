1417b5121e60f86c15d98c065c5d57da48c92f4d
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-22 12:57:59 +0100

    Modificamos los test para que sirvan para evento

--- hgt/tests/test_evento.py
+++ hgt/tests/test_evento.py
@@ -5,14 +5,14 @@ from hgt.evento import Evento
 def evento():
     return Evento(28, 10, 30, 12)
 
-def test_read(tarea):
-    assert tarea.read() == "28|&&|10|&&|30|&&|12"
+def test_read(evento):
+    assert evento.read() == "28|&&|10|&&|30|&&|12"
 
-def test_update(tarea):
-    tarea.update(28, 11, 30, 11)
+def test_update(evento):
+    evento.update(28, 11, 30, 11)
 
-    assert tarea.read() == "28|&&|11|&&|30|&&|11"
+    assert evento.read() == "28|&&|11|&&|30|&&|11"
 
-def test_delete(tarea):
-    tarea.delete()
-    assert tarea.read() == "None|&&|None|&&|None|&&|None"
+def test_delete(evento):
+    evento.delete()
+    assert evento.read() == "None|&&|None|&&|None|&&|None"
