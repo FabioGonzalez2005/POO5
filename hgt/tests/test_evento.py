b2fe418a2ed7fe4d9017e55fb4f7ca3dc83e8226
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-24 09:52:20 +0100

    Cambiamos los test

--- hgt/tests/test_evento.py
+++ hgt/tests/test_evento.py
@@ -3,15 +3,15 @@ from hgt.evento import Evento
 
 @pytest.fixture
 def evento():
-    return Evento(28, 10, 30, 12)
+    return Evento("28-10-2024", "10:20", "30-10-2024", "12:00")
 
 def test_read(evento):
-    assert evento.read() == "28|&&|10|&&|30|&&|12"
+    assert evento.read() == "28-10-2024|&&|10:20|&&|30-10-2024|&&|12:00"
 
 def test_update(evento):
     evento.update(28, 11, 30, 11)
 
-    assert evento.read() == "28|&&|11|&&|30|&&|11"
+    assert evento.read() == "28-10-2024|&&|10:20|&&|30-10-2024|&&|12:00"
 
 def test_delete(evento):
     evento.delete()
