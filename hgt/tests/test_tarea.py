7a2d5fb4a208bf4c8b9ad4a858c8da92920d53d5
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-24 09:54:36 +0100

    Cambiamos los tipos de comillas

--- hgt/tests/test_tarea.py
+++ hgt/tests/test_tarea.py
@@ -3,13 +3,13 @@ from hgt.tarea import Tarea
 
 @pytest.fixture
 def tarea():
-    return Tarea(1, 'Mates', False)
+    return Tarea(1, "Mates", False)
 
 def test_read(tarea):
     assert tarea.read() == "1|&&|Mates|&&|False"
 
 def test_update(tarea):
-    tarea.update(2, 'Lengua', True)
+    tarea.update(2, "Lengua", True)
 
     assert tarea.read() == "2|&&|Lengua|&&|True"
 
