d650e2cfaea7e76280eab85f2975fc8b1568571f
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-24 10:01:07 +0100

    Realizamos los test de delete y solucionamos el test de update

--- hgt/tests/test_evento.py
+++ hgt/tests/test_evento.py
@@ -9,10 +9,10 @@ def test_read(evento):
     assert evento.read() == "1|&&|Mates|&&|False|&&|28-10-2024|&&|10:20|&&|30-10-2024|&&|12:00"
 
 def test_update(evento):
-    evento.update(28, 11, 30, 11)
+    evento.update(1, "Mates II", False, "29-10-2024", "11:20", "31-10-2024", "13:00")
 
-    assert evento.read() == "28-10-2024|&&|10:20|&&|30-10-2024|&&|12:00"
+    assert evento.read() == "1|&&|Mates II|&&|False|&&|29-10-2024|&&|11:20|&&|31-10-2024|&&|13:00"
 
 def test_delete(evento):
     evento.delete()
-    assert evento.read() == "None|&&|None|&&|None|&&|None"
+    assert evento.read() == "None|&&|None|&&|None|&&|None|&&|None|&&|None|&&|None"
