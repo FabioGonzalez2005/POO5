89260a37bb6e8ef118e8c6fb7996fbc962c98b7b
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-24 09:31:26 +0100

    Establecemos el tipo de dato que queremos que entre

--- hgt/evento.py
+++ hgt/evento.py
@@ -1,7 +1,7 @@
 from hgt.tarea import Tarea
 
 class Evento(Tarea):
-    def __init__(self, fechaInicio, horaInicio, fechaFin, horaFin):
+    def __init__(self, fechaInicio:str, horaInicio:str, fechaFin:str, horaFin:str):
         self.fechaInicio = fechaInicio
         self.horaInicio  = horaInicio
         self.fechaFin    = fechaFin
@@ -11,7 +11,7 @@ class Evento(Tarea):
     def read(self):
         return f'{self.fechaInicio}|&&|{self.horaInicio}|&&|{self.fechaFin}|&&|{self.horaFin}'
 
-    def update(self, fechaInicio, horaInicio, fechaFin, horaFin):
+    def update(self, fechaInicio:str, horaInicio:str, fechaFin:str, horaFin:str):
         self.fechaInicio = fechaInicio
         self.horaInicio  = horaInicio
         self.fechaFin    = fechaFin
