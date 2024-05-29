12a52408fe48ca165abdd009bd8528a6878813e2
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-05-29 09:49:07 +0100

    hacemos que los dos botones sean horizontales

--- textual2/button.py
+++ textual2/button.py
@@ -4,26 +4,11 @@ from textual.widgets import Button, Static
 
 
 class ButtonsApp(App[str]):
-    CSS_PATH = "button.tcss"
 
     def compose(self) -> ComposeResult:
         yield Horizontal(
-            VerticalScroll(
-                Static("Standard Buttons", classes="header"),
-                Button("Default"),
-                Button("Primary!", variant="primary"),
-                Button.success("Success!"),
-                Button.warning("Warning!"),
-                Button.error("Error!"),
-            ),
-            VerticalScroll(
-                Static("Disabled Buttons", classes="header"),
-                Button("Default", disabled=True),
-                Button("Primary!", variant="primary", disabled=True),
-                Button.success("Success!", disabled=True),
-                Button.warning("Warning!", disabled=True),
-                Button.error("Error!", disabled=True),
-            ),
+                Button("Aceptar"),
+                Button("Cancelar"),
         )
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
